c9c02ea485872d4f86dfa9fa167d9b2db9b4a978
mrmammadov <qulu.m@code.edu.az>
2021-05-17 17:10:46 +0200

    Default Picture Bug - FIXED

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -134,4 +133,4 @@ LOGIN_REDIRECT_URL = '/users/profile/'
 LOGIN_URL = '/users/login/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/profile_pics/'
+MEDIA_URL = '/media/'
