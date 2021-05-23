892fec5a288bc8d4f1a4399bd54b183db44d0235
Qulu Mammadli <mammadov.qulu@gmail.com>
2021-05-24 00:47:36 +0200

    All CRUD operations are changed to Class-based Views

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
-from blogs.views import PostListView
+from blogs.views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blogs-home'),
+    path('posts/new/', PostCreateView.as_view(), name='blogs-create'),
+    path('posts/<int:pk>', PostDetailView.as_view(), name='blogs-detail'),
+    path('posts/<int:pk>/update', PostUpdateView.as_view(), name='blogs-update'),
+    path('posts/<int:pk>/delete', PostDeleteView.as_view(), name='blogs-delete'),
     path('about/', views.about, name='blogs-about'),
 ]
